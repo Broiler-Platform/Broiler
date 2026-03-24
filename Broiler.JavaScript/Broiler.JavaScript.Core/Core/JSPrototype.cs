812492b0b3b9dce5d015cace71195f23ac48ae81
MaiRat <maik.ratzmer@gmail.com>
2026-03-24 13:08:48 +0100

    Cleanup

--- Broiler.JavaScript/Broiler.JavaScript.Core/Core/JSPrototype.cs
+++
@@ -1,3 +0,0 @@
-// JSPrototype has been moved to the Broiler.JavaScript.BuiltIns assembly.
-// Core now uses the IJSPrototype interface (in Runtime) and a factory
-// delegate (JSObject.CreatePrototype) wired by BuiltInsAssemblyInitializer.
