60082684a8cf8e19b3e9b499a226e985f85a3fa9
MaiRat <maik.ratzmer@gmail.com>
2026-03-24 13:06:48 +0100

    Cleanup

--- Broiler.JavaScript/Broiler.JavaScript.Core/Core/DisposableAction.cs
+++
@@ -1,3 +0,0 @@
-// CancellableDisposableAction has been moved to Broiler.JavaScript.Runtime.
-// See Broiler.JavaScript.Runtime/CancellableDisposableAction.cs.
-// TypeForwardedTo attribute in AssemblyInfo.cs preserves binary compatibility.
