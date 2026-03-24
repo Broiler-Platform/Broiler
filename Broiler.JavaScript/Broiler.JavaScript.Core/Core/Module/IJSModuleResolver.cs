1a2a0dc4bb4de8f9ef7dd19e3ccf424d1d9f5e32
MaiRat <maik.ratzmer@gmail.com>
2026-03-24 13:42:47 +0100

    Cleanup

--- Broiler.JavaScript/Broiler.JavaScript.Core/Core/Module/IJSModuleResolver.cs
+++
@@ -1,3 +0,0 @@
-// IJSModuleResolver has been moved to Broiler.JavaScript.Runtime.
-// The type forward in AssemblyInfo.cs ensures binary compatibility.
-// See: Broiler.JavaScript.Runtime/IJSModuleResolver.cs
